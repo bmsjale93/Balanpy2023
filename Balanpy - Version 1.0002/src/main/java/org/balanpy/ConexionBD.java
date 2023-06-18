096ab668f5745f58e7ae5dd49be4a355be303717
Brenda Solben <83696510+solben@users.noreply.github.com>
2023-06-18 18:23:16 +0200

    Update ConexionBD.java

--- Balanpy - Version 1.0002/src/main/java/org/balanpy/ConexionBD.java
+++ Balanpy - Version 1.0002/src/main/java/org/balanpy/ConexionBD.java
@@ -4,29 +4,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import org.apache.commons.dbcp2.BasicDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 public class ConexionBD {
 	
 	    //Indicamos donde está la bd, en este caso en localhost/puerto/nombre de la bd que creamos en workbench. 
 		private static final String JDBC_URL = "jdbc:mysql://localhost:3306/database_balanpy";
-		private static Connection instance = null;
+		//Establecer pool de conexiones para reutilizar las conexiones existentes 
+		private static BasicDataSource ds = null;
 		
 		private ConexionBD() {
 			
 		}
 		
 		public static Connection getConnection() throws SQLException {
-			if (instance == null) {
-				//Cadenas de valores
-				Properties props = new Properties();
-				//Estructura de clave:valor
-				props.put("user", "root");
-				props.put("password", "");
-				//Establecemos la conexión a través de estos parámetros(ruta db, nombre usuario, contrasenia) para acceder a la db.
-				instance = DriverManager.getConnection(JDBC_URL, props);
-				System.out.println("Successful connection to the database");
-			}
-			return instance;
+			
+				if (ds == null) {
+					
+					String user = "root";
+					String password = "";
+					
+					ds = new BasicDataSource();
+					ds.setUrl(JDBC_URL);
+		            ds.setUsername(user);
+		            ds.setPassword(password);
+		            
+					System.out.println("Successful connection to the database");
+				}
+				return ds.getConnection();//Primera creación del pool.
 		}
-
 }
