e56925b8036629c3b859d55ad6fae087fc1f20cf
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2023-06-18 13:34:10 +0200

    Delete balanpy-main directory

--- balanpy-main/src/main/java/org/balanpy/PerfilVeterinario.java
+++
@@ -1,4 +0,0 @@
-package org.balanpy;
-
-public class PerfilVeterinario {
-}
--- balanpy-main/src/main/java/org/balanpy/UsuarioException.java
+++
@@ -1,9 +0,0 @@
-package org.balanpy;
-
-public class UsuarioException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public UsuarioException(String error) {
-		super(error);
-	}
-}
